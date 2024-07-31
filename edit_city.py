d72bd4dfef80331287bf6d4e58662c022410b3e9
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-07-31 11:26:19 -0500

    feat: :sparkles: creacion de la funcion edi_city
    esta funcion sirve para editar los datos de las ciudades registradas
    
    @Kevinromero04

---
+++ edit_city.py
@@ -0,0 +1,44 @@
+import carga_guarda as gg
+def menu():
+    menu = ["1. Para Nombre de la ciudad", "2. Para la poblacion de la ciudad", "3. Para el pais", "4. Salir"]
+    for n in menu:
+        print(n)
+
+
+def editar_city():
+    while True:
+        data = gg.cargar_city()
+        while True:
+            try:
+                cd_postal = int(input("Digite el codigo postal de la ciudad que vas a editar presiona 0 si quieres salir: "))
+                break
+            except Exception:
+                print("Digite bien los datos")
+    
+        if str(cd_postal) in data["Ciudades"]:
+            while True:
+                menu()
+                op = int(input("Seleccione una opcion: "))
+                if op == 1:
+                    nuevo = str(input("Digite el nuevo nombre de la ciudad: "))
+                    data["Ciudades"][str(cd_postal)]["Nombre ciudad"] = nuevo
+                    gg.guardar_city(data)
+                    
+                elif op == 2:
+                    nuevo = int(input("Digite la nuevo poblacion de la ciudad: "))
+                    data["Ciudades"][str(cd_postal)]["Poblacion"] = nuevo
+                    gg.guardar_city(data)
+                    
+                elif op == 3:
+                    nuevo = int(input("Digite pais de la ciudad: "))
+                    data["Ciudades"][str(cd_postal)]["Pais"] = nuevo
+                    gg.guardar_city(data)
+                    
+                elif op == 4:
+                    print("Saliendo...")
+                    break
+            break
+        elif cd_postal == 0:
+            print("Saliendo...")
+            break
+        
