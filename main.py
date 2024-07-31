57aef550d701513219c9623dd43a23eae1bf7360
mariana34r <cesaraugustoruedarangel@gmail.com>
2024-07-31 10:45:08 -0500

    feat: :sparkles: avance en el menu

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print  ("holaa")
+import letreros
+
+print("Binevenido :)")
+print("Ingrese su Nombre Usuario")
+Nombre=input("-")
+print("Binevenido",Nombre)
+while True:
+    print("Que desea hacer ")
+    print("\n.1 Crear cuidades\n2.Editar Cuidades\n3.Mostrar Cuidades\n4.Salir")
+    print("Escoga una Opcion")
+    try:
+        opc = input("-")
+        if opc == '1':
+           
+        elif opc == '2':
+           
+        elif opc == '3':
+            
+           
+        elif opc == '4':
+           
+            break  
+        else:
+            print("Opción no válida. Por favor, ingrese un número entre 1 y 5.")
+    
+    except Exception :
+        print("Ingrese una opcion valida")
+        
+        
